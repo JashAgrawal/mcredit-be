13ca12b9926abcccafc95437d6ebe043f0c27417
Jash Agrawal <agrawaljash99@gmail.com>
2024-03-04 00:10:44 +0530

    init

--- router/index.ts
+++ router/index.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import {
+  addCustomer,
+  getCustomerByUserIdAndCustomerId,
+  getCustomersByUserId,
+} from "../controller/customer";
+import { addTransaction } from "../controller/transaction";
 const router = Router();
 
-router.post("/add-customer");
-router.get("/get-customers");
-router.post("/add-transaction");
-router.get("/get-transactions");
+router.post("/add-customer", addCustomer);
+router.get("/get-customers", getCustomersByUserId);
+router.post("/add-transaction", addTransaction);
+router.get("/get-transactions", getCustomerByUserIdAndCustomerId);
 
 export default router;
